ccf3b9817f49c21b07492da311400e90b046c2f4
Eason <77266837@qq.com>
2025-08-16 13:18:00 +0800

    Refactor Game class to use Lombok getters/setters
    Replaced manual getter and setter methods in the Game class with Lombok's @Getter and @Setter annotations to reduce boilerplate and improve code maintainability.

--- src/main/java/nextplay/cn/deathSwap/Game.java
+++ src/main/java/nextplay/cn/deathSwap/Game.java
@@ -1,5 +1,7 @@
 package nextplay.cn.deathSwap;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
@@ -12,6 +14,8 @@ import org.bukkit.entity.Player;
  *
  * @author jonathan [admin@56fkj.cn]
  */
+@Getter
+@Setter
 public class Game {
 
     public enum GameState {
@@ -35,61 +39,5 @@ public class Game {
         this.countdown = 60;
         this.readyTime = 60;
         this.timeBar = Bukkit.createBossBar("DeathSwap", BarColor.GREEN, BarStyle.SEGMENTED_20);
-    }
-
-    public GameState getState() {
-        return state;
-    }
-
-    public void setState(GameState state) {
-        this.state = state;
-    }
-
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
-
-    public Player getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Player creator) {
-        this.creator = creator;
-    }
-
-    public int getCountdown() {
-        return countdown;
-    }
-
-    public void setCountdown(int countdown) {
-        this.countdown = countdown;
-    }
-
-    public int getReadyTime() {
-        return readyTime;
-    }
-
-    public void setReadyTime(int readyTime) {
-        this.readyTime = readyTime;
-    }
-
-    public BossBar getTimeBar() {
-        return timeBar;
-    }
-
-    public void setTimeBar(BossBar timeBar) {
-        this.timeBar = timeBar;
     }
 }
